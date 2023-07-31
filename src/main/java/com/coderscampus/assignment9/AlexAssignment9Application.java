3670d5cc1954a877eb40ad89927dcdc54f4f34a0
amamolo9986 <amamolo9986@yahoo.com>
2023-07-31 08:16:03 -0500

    Initial commit

---
+++ src/main/java/com/coderscampus/assignment9/AlexAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexAssignment9Application.class, args);
+	}
+
+}
